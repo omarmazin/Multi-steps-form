5386736e0ea2ba7f1d19234cc44acf503974c8fc
omarmazin <omarbawazir42@gmail.com>
2023-08-16 16:01:02 +0300

    Working on the second page and the scroller

--- src/Components/page2.js
+++ src/Components/page2.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react"
 import "./page2.css"
+import iconArcade from "../assets/icon-arcade.svg"
+import iconAdvanced from "../assets/icon-advanced.svg"
+import iconPro from "../assets/icon-pro.svg"
+import Toggle from 'react-toggle';
+
 export default function Page2(){
     const [items,setItems] = useState([{
         id:1,
@@ -23,6 +28,27 @@ export default function Page2(){
         step:4,
         paragraph:"SUMMARY",
     }])
+    const [plan,setPlan] = useState([{
+        plan:"Arcade",
+        yearlyPrice:90,
+        monthlyPrice:9,
+        icon:iconArcade
+    },{
+        plan:"Advanced",
+        yearlyPrice:120,
+        monthlyPrice:12,
+        icon:iconAdvanced
+    },{
+        plan:"Pro",
+        yearlyPrice:150,
+        monthlyPrice:15,
+        icon:iconPro
+    }
+    ])
+    const [isToggled, setIsToggled] = useState(false);
+    const handleToggle = () => {
+        setIsToggled(!isToggled);
+      };
     return(
         <div className="page2">
             <aside className="asideBar2">
@@ -44,8 +70,34 @@ export default function Page2(){
                     }
                 </nav>
             </aside>
-            <main className="mainContainer">l
-                    dsewew
+            <main className="mainContainer">
+                <p className="head">Select your plan</p>
+                <p className="head-text">You have the option of monthly or yearly billing.</p>
+                <div className="plan-container">
+                    {
+                        plan.map((plan,index)=>(
+                            <div className="card">
+                                <img src={plan.icon} alt={plan.plan}/>
+                                <p className="planName">{plan.plan}</p>
+                                <p className="planPrice">${plan.monthlyPrice}/mo</p>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className="toggle-labels">
+                    <span>Montly</span>
+                    <label className="toggle-switch">
+                        <input type="checkbox" checked={isToggled} onChange={handleToggle} />
+                        <span className="slider"></span>
+                    </label>
+                    <span>Yearly</span>
+                </div>
+
+                
+            {/* {isToggled && 
+            <p className="planPrice" >${plan.monthlyPrice}/yr</p>
+            
+            } */}
             </main>
         </div>
     )
