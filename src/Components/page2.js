dde1aa04275ffbcda4e36e51e156d4cacd1ec630
omarmazin <omarbawazir42@gmail.com>
2023-08-20 20:36:02 +0300

    Fixing Selector problem ( Styling & Logically )

--- src/Components/page2.js
+++ src/Components/page2.js
@@ -29,16 +29,19 @@ export default function Page2(){
         paragraph:"SUMMARY",
     }])
     const [plan,setPlan] = useState([{
+        id:1,
         plan:"Arcade",
         yearlyPrice:90,
         monthlyPrice:9,
         icon:iconArcade
     },{
+        id:2,
         plan:"Advanced",
         yearlyPrice:120,
         monthlyPrice:12,
         icon:iconAdvanced
     },{
+        id:3,
         plan:"Pro",
         yearlyPrice:150,
         monthlyPrice:15,
@@ -49,6 +52,68 @@ export default function Page2(){
     const handleToggle = () => {
         setIsToggled(!isToggled);
       };
+      const [selectedChoice, setSelectedChoice] = useState(null); // Initialize with no choice selected
+      const handleChoiceClick = (index) => {
+        setSelectedChoice(index); 
+      };
+      
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        const price = window.localStorage.setItem("Price","")
+        const plan = window.localStorage.setItem("Plan","")
+      }
+      
+      const [clickedDivId, setClickedDivId] = useState(null);
+
+      const handleClickMonthly = (event) => {
+        const clickedId = event.target.id;
+        setClickedDivId(clickedId);
+        console.log('Clicked div id:', clickedId);
+        switch (clickedId){
+            case '1':
+                window.localStorage.setItem("Price","9$")
+                window.localStorage.setItem("Plan","mo")
+                break;
+            case '2':
+                window.localStorage.setItem("Price","12$")
+                window.localStorage.setItem("Plan","mo")
+                break;
+            case '3':
+                window.localStorage.setItem("Price","15$")
+                window.localStorage.setItem("Plan","mo")
+                break;
+            default:
+                console.error("Not Found");
+        }
+
+
+      };
+      const handleClickYearly = (event) =>{
+        const clickedId = event.target.id;
+        setClickedDivId(clickedId);
+        console.log('Clicked div id: ', clickedId);
+        switch (clickedId){
+            case '1':
+                window.localStorage.setItem("Price","90$")
+                window.localStorage.setItem("Plan","yr")
+                break;
+            case '2':
+                window.localStorage.setItem("Price","120$")
+                window.localStorage.setItem("Plan","yr")
+                break;
+            case '3':
+                window.localStorage.setItem("Price","150$")
+                window.localStorage.setItem("Plan","yr")
+                break;
+            default:
+                console.error("Not Found");
+        }
+
+    } 
+    
+      
+
+
     return(
         <div className="page2">
             <aside className="asideBar2">
@@ -71,33 +136,40 @@ export default function Page2(){
                 </nav>
             </aside>
             <main className="mainContainer">
-                <p className="head">Select your plan</p>
-                <p className="head-text">You have the option of monthly or yearly billing.</p>
-                <div className="plan-container">
-                    {
-                        plan.map((plan,index)=>(
-                            <div className="card">
-                                <img src={plan.icon} alt={plan.plan}/>
-                                <p className="planName">{plan.plan}</p>
-                                <p className="planPrice">${plan.monthlyPrice}/mo</p>
-                            </div>
-                        ))
-                    }
-                </div>
-                <div className="toggle-labels">
-                    <span>Montly</span>
-                    <label className="toggle-switch">
-                        <input type="checkbox" checked={isToggled} onChange={handleToggle} />
-                        <span className="slider"></span>
-                    </label>
-                    <span>Yearly</span>
-                </div>
+                <form onSubmit={handleSubmit}>
+                    <p className="head">Select your plan</p>
+                    <p className="head-text">You have the option of monthly or yearly billing.</p>
+                    <div className="plan-container">
+                        {
+                            
+                            plan.map((plan,index)=>(
+                                <div key={index} className="card" onClick={() => handleChoiceClick(index) } style={{border: selectedChoice === index ? '1px solid blueviolet' : 'none', }}
+                                >
+                                    <img src={plan.icon} alt={plan.plan}/>
+                                    <p className="planName" >{plan.plan}</p>
+                                    
+                                    {
+                                        isToggled || <p id={plan.id} onClick={handleClickMonthly} className="planPrice" >${plan.monthlyPrice}/mo</p>
 
-                
-            {/* {isToggled && 
-            <p className="planPrice" >${plan.monthlyPrice}/yr</p>
-            
-            } */}
+                                    }
+                                    {isToggled&&
+                                    <p id={plan.id} onClick={handleClickYearly} className="planPrice">${plan.yearlyPrice}/yr</p>   
+                                                                
+                                    }
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <div className="toggle-labels">
+                        <span>Montly</span>
+                        <label className="toggle-switch">
+                                        
+                            <input type="checkbox" checked={isToggled} onChange={handleToggle} />
+                            <span className="slider"></span>
+                        </label>
+                        <span>Yearly</span>
+                    </div>
+                </form>
             </main>
         </div>
     )
